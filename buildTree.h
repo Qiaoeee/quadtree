6f4de71cf828143305c6e1f26d2b97cb237cb25b
Qiaoeee <45564338+Qiaoeee@users.noreply.github.com>
2018-12-12 10:58:33 +0800

    add buildTree files

---
+++ buildTree.h
@@ -0,0 +1,9 @@
+#ifndef BUILDTREE_H_INCLUDED
+#define BUILDTREE_H_INCLUDED
+
+#include "treeStructure.h"
+extern Node *makeNode( double x, double y, int level );
+extern void makeChildren( Node *parent );
+
+#endif // BUILDTREE_H_INCLUDED
+

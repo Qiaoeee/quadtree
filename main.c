95aa0c52fe6a0fcdd0c740f406af8fc3bcd4556c
Qiaoeee <45564338+Qiaoeee@users.noreply.github.com>
2018-12-12 10:47:40 +0800

    add files

---
+++ main.c
@@ -0,0 +1,30 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include "buildTree.h"
+#include "writeTree.h"
+#include "treeStructure.h"
+// main
+
+
+int main( int argc, char **argv ) {
+
+  Node *head;
+
+  // make the head node
+  head = makeNode( 0.0,0.0, 0 );
+
+  // make a tree
+  makeChildren( head );
+  makeChildren( head->child[1] );
+  makeChildren( head->child[3] );
+  makeChildren( head->child[2] );
+  makeChildren( head->child[3]->child[3]);
+  makeChildren( head->child[3]->child[3]->child[2]);
+  makeChildren( head->child[2]->child[1]);
+  makeChildren( head->child[2]->child[1]->child[0]);
+
+  // print the tree for Gnuplot
+	writeTree( head );
+
+  return 0;
+}
